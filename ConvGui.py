11f59c4884bed984a0f453da4ddb6f928e339e15
Sam Condon <samcondon44@gmail.com>
2019-11-10 12:57:30 -0800

    GUI functioning and playing the user input specified sample

--- ConvGui.py
+++ ConvGui.py
@@ -94,79 +94,100 @@ class DisplayRecording(tk.Frame):
         self.parent = parent_
         self.controller = controller_
         
+        self.samplist = []
+        self.entry = tk.StringVar()
+        
+        ##tuple to store current signal to be played or to convolute##
+        self.cursignal = ()
+
         self.recording = np.zeros(recordtime*44100)
         self.recsamps = np.arange(0,recordtime*44100,1)
+        self.samplist.append((self.recsamps,self.recording))
         self.RECplot = f.add_subplot(gs[0,2])
-        self.RECplot.title.set_text('Current Waveform')
-        
+        self.RECplot.title.set_text('(0) Current Waveform')
+        self.RECplot.plot(self.recsamps,self.recording)
+
         self.MLdata = ReadWav('Masonic Lodge.wav')
         self.MLsamps = np.arange(0,len(self.MLdata),1)
+        self.samplist.append((self.MLsamps,self.MLdata))
         self.MLplot = f.add_subplot(gs[1,0])
-        self.MLplot.title.set_text('Masonic Lodge')
+        self.MLplot.title.set_text('(1) Masonic Lodge')
         self.MLplot.plot(self.MLsamps,self.MLdata)
 
         self.NDRdata = ReadWav('Nice Drum Room.wav')
         self.NDRsamps = np.arange(0,len(self.NDRdata),1)
+        self.samplist.append((self.NDRsamps,self.NDRdata))
         self.NDRplot = f.add_subplot(gs[1,1])
-        self.NDRplot.title.set_text('Nice Drum Room')
+        self.NDRplot.title.set_text('(2) Nice Drum Room')
         self.NDRplot.plot(self.NDRsamps,self.NDRdata)
 
         self.PGdata = ReadWav('Parking Garage.wav')
         self.PGsamps = np.arange(0,len(self.PGdata),1)
+        self.samplist.append((self.PGsamps,self.PGdata))
         self.PGplot = f.add_subplot(gs[1,2])
-        self.PGplot.title.set_text('Parking Garage')
+        self.PGplot.title.set_text('(3) Parking Garage')
         self.PGplot.plot(self.PGsamps,self.PGdata)
 
         self.RGTdata = ReadWav('Right Glass Triangle.wav')
         self.RGTsamps = np.arange(0,len(self.RGTdata),1)
+        self.samplist.append((self.RGTsamps,self.RGTdata))
         self.RGTplot = f.add_subplot(gs[1,3])
-        self.RGTplot.title.set_text('Right Glass Triangle')
+        self.RGTplot.title.set_text('(4) Right Glass Triangle')
         self.RGTplot.plot(self.RGTsamps,self.RGTdata)
 
         self.SCONVdata = np.zeros(recordtime*44100)
         self.SCONVsamps = np.arange(0,recordtime*44100)
+        self.samplist.append((self.SCONVsamps,self.SCONVdata))
         self.SCONVplot = f.add_subplot(gs[1,4])
-        self.SCONVplot.title.set_text('Record Your Own!')
+        self.SCONVplot.title.set_text('(5) Record Your Own!')
 
         self.speaker = sc.get_speaker('Built-in')
         ####################################
 
         tk.Frame.__init__(self,parent_)
 
-        label = tk.Label(self, text="Waveform", font=LARGE_FONT)
-        label.grid(row=0, column=1, padx=10, pady=10)
-
         ##BUTTONS###################################################################################
-        homeret = tk.Button(self, text="Return to Home", command=lambda: self.ret_home())
-        homeret.grid(row=2,column=0)
+        homeret = tk.Button(self, text="Return to Home", activebackground='blue',height=10, command=lambda: self.ret_home())
+        homeret.grid(row=2,column=7)
 
-        playrec = tk.Button(self, text="Play Recording", command=lambda: self.play_recording())
-        playrec.grid(row=3,column=2)
+        playrec = tk.Button(self, text="Play Recording", height=10, command=lambda: self.play_recording())
+        playrec.grid(row=1,column=7)
+
+        entry = tk.Entry(self, textvariable=self.entry) 
+        entry.grid(row=1, column=5)
         ############################################################################################
         
         ##PLOTS##############################################
+                
         
-
         #####################################################
 
         canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
         canvas.get_tk_widget().grid(row=1,column=1)
 
-    #Pass a list or numpy array
     def set_recording(self,rec):
         self.recording = np.array(rec)
+        self.samplist[0] = (self.recsamps,self.recording)
+
+    def set_cursignal(self):
+        inpt = self.entry.get()
+        
+        if(inpt.isdigit() and int(inpt)>=0 and int(inpt)<=5):
+            self.cursignal = self.samplist[int(inpt)]
+        else:
+            self.cursignal = self.samplist[0]
 
     def ret_home(self):
         self.controller.show_frame(StartPage)
     
     def play_recording(self):
-        self.speaker.play(self.recording, samplerate=44100)
+        self.set_cursignal()
+        self.speaker.play(self.cursignal[1], samplerate=44100)
 
     def show_waveform(self,k):
         self.RECplot.clear()
         self.RECplot.plot(self.recsamps, self.recording)
-
 
 ###################################################################################
 
